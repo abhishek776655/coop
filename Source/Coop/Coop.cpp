b2abec95438afa5bf9fb72c22403b027e6bfb7c8
Abhishek Pandey <abhishek.pandey9868@gmail.com>
2021-02-28 15:05:51 +0530

    init commit

---
+++ Source/Coop/Coop.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Coop.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Coop, "Coop" );
---
+++ Source/Coop/CoopGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CoopGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COOP_API ACoopGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
